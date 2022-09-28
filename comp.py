e95cfab9f9bff52e11b56d20f7ca5fb7c4349361
Ranjithkannanmanohar <37570624+Ranjithkannanmanohar@users.noreply.github.com>
2022-09-28 10:48:08 +0100

    Add files via upload

---
+++ comp.py
@@ -0,0 +1,24 @@
+import difflib
+import time
+
+with open('devices.txt') as routers:
+    lines = routers.readlines()
+
+IP = 192.168.10.
+
+def compare():
+    show_pre = "Show_pre"
+    show_post = "Show_post"
+    show_pre_lines = open("./" +IP + "_pre.txt").readlines()
+    time.sleep(0.5)
+    show_post_lines = open("./" +IP + "_post.txt").readlines()
+    time.sleep( 0.5 )
+    difference = difflib.HtmlDiff(wrapcolumn=80).make_file(show_pre_lines, show_post_lines,show_pre,show_post)
+    difference_report = open("./show_comparison.html", "w+")
+    difference_report.write(difference)
+    difference_report.close()
+    time.sleep(0.5)
+
+compare()
+
+print("Task Completed.")
