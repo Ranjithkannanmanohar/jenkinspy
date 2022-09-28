a35f54e5b452bc839a0b8bbb656653947db8f640
Ranjithkannanmanohar <37570624+Ranjithkannanmanohar@users.noreply.github.com>
2022-09-28 10:55:34 +0100

    Update comp.py

--- comp.py
+++ comp.py
@@ -13,7 +13,7 @@ def compare():
     show_post_lines = open("./" +IP + "_post.txt").readlines()
     time.sleep( 0.5 )
     difference = difflib.HtmlDiff(wrapcolumn=80).make_file(show_pre_lines, show_post_lines,show_pre,show_post)
-    difference_report = open("./show_comparison.html", "w+")
+    difference_report = open("./show_comparison.xml", "w+")
     difference_report.write(difference)
     difference_report.close()
     time.sleep(0.5)
